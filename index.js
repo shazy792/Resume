552da85973575c4ddbc0b01902b7b7722d4dc0c7
Shahzil Aftab <Shahzil Aftab>
2016-07-09 02:27:38 -0500

    Better Code for postback catcher

--- index.js
+++ index.js
@@ -79,73 +79,76 @@ app.post('/webhook', (req, res) => {
   // Parsing the Messenger API response
   const messaging = FB.getFirstMessagingEntry(req.body); // Get Postback Event, Check Webhook array
 
-  if (messaging.postback){
-  	const sender = messaging.sender.id;
-  	console.log("Postback recieved and Ignored");
-  	console.log(messaging.postback["payload"]);
-  	let mes = {text: 'Postback Recived!'};
-  	FB.fbMessage(
-  		sender,
-  		mes
-  		);
-  	console.log("Here");
+  if (messaging){
 
-  }
-
-  if (messaging && messaging.message) {
-
-    // Yay! We got a new message!
-
-    // We retrieve the Facebook user ID of the sender
+  	// We retrieve the Facebook user ID of the sender
     const sender = messaging.sender.id;
 
-    // We retrieve the user's current session, or create one if it doesn't exist
-    // This is needed for our bot to figure out the conversation history
-    const sessionId = findOrCreateSession(sender);
+	  if (messaging.postback){
+	  	const sender = messaging.sender.id;
+	  	console.log("Postback recieved and Ignored");
+	  	console.log(messaging.postback["payload"]);
+	  	let mes = {text: 'Postback Recived!'};
+	  	FB.fbMessage(
+	  		sender,
+	  		mes
+	  		);
+	  	console.log("Here");
 
-    // We retrieve the message content
-    const msg = messaging.message.text;
-    const atts = messaging.message.attachments;
-    
-    if (atts) {
-      // We received an attachment
+	  } 
 
-      // Let's reply with an automatic message
-      let mes = {text:'Sorry I can only process text messages for now.'}
-      FB.fbMessage(
-        sender,
-        mes
-      );
-    } else if (msg) {
-      // We received a text message
+	  else if (messaging.message) {
 
-      // Let's forward the message to the Wit.ai Bot Engine
-      // This will run all actions until our bot has nothing left to do
-      wit.runActions(
-        sessionId, // the user's current session
-        msg, // the user's message 
-        sessions[sessionId].context, // the user's current session state
-        (error, context) => {
-          if (error) {
-            console.log('Oops! Got an error from Wit:', error);
-          } else {
-            // Our bot did everything it has to do.
-            // Now it's waiting for further messages to proceed.
-            console.log('Waiting for futher messages.');
+	    // Yay! We got a new message.
 
-            // Based on the session state, you might want to reset the session.
-            // This depends heavily on the business logic of your bot.
-            // Example:
-            // if (context['done']) {
-            //   delete sessions[sessionId];
-            // }
+	    // We retrieve the user's current session, or create one if it doesn't exist
+	    // This is needed for our bot to figure out the conversation history
+	    const sessionId = findOrCreateSession(sender);
 
-            // Updating the user's current session state
-            sessions[sessionId].context = context;
-          }
-        }
-      );
-    }
-  }
-  res.sendStatus(200);
+	    // We retrieve the message content
+	    const msg = messaging.message.text;
+	    const atts = messaging.message.attachments;
+	    
+	    if (atts) {
+	      // We received an attachment
+
+	      // Let's reply with an automatic message
+	      let mes = {text:'Sorry I can only process text messages for now.'}
+	      FB.fbMessage(
+	        sender,
+	        mes
+	      );
+	    } else if (msg) {
+	      // We received a text message
+
+	      // Let's forward the message to the Wit.ai Bot Engine
+	      // This will run all actions until our bot has nothing left to do
+	      wit.runActions(
+	        sessionId, // the user's current session
+	        msg, // the user's message 
+	        sessions[sessionId].context, // the user's current session state
+	        (error, context) => {
+	          if (error) {
+	            console.log('Oops! Got an error from Wit:', error);
+	          } else {
+	            // Our bot did everything it has to do.
+	            // Now it's waiting for further messages to proceed.
+	            console.log('Waiting for futher messages.');
+
+	            // Based on the session state, you might want to reset the session.
+	            // This depends heavily on the business logic of your bot.
+	            // Example:
+	            // if (context['done']) {
+	            //   delete sessions[sessionId];
+	            // }
+
+	            // Updating the user's current session state
+	            sessions[sessionId].context = context;
+	          }
+	        }
+	      );
+	    }
+	  }
+	}
+	  res.sendStatus(200);
 });
