e6c4d94d4d2334b716492e2065e045642b7420d9
Shahzil Sheikh <shazy792@hotmail.com>
2016-07-13 16:40:22 -0700

    Added Privacy Policy

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const bot = require('./bot.js');
 const Config = require('./const.js');
 const FB = require('./facebook.js');
 const dat = require('./data.js');
+const ppolicy = require('./privacypolicy.html');
 
 // Setting up our bot
 const wit = bot.getWit();
@@ -60,6 +61,11 @@ console.log("I'm wating for you @" + PORT);
 // index. Let's say something fun
 app.get('/', function(req, res) {
   res.send('"Only those who will risk going too far can possibly find out how far one can go." - T.S. Eliot');
+});
+
+// index. Let's say something fun
+app.get('/privacy', function(req, res) {
+  res.sendFile(ppolicy);
 });
 
 // Webhook verify setup using FB_VERIFY_TOKEN
